812617262f25b92d60b27d4a2a8da6ad29ab860d
Deivid30Medina <deivis32sebas02@gmail.com>
2025-01-01 23:12:59 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Frontend-Cloud-Technical-Test-Deploy"
+  base: "/Frontend-Cloud-Technical-Test-Deploy/task"
 })
